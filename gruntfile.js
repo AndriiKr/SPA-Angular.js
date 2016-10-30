49d5e5ffc620dd553789a22d7d29451e962851d3
AndriiKr <1drun1@gmail.com>
2016-10-30 11:15:59 +0200

    blocks ready

--- gruntfile.js
+++ gruntfile.js
@@ -1,4 +1,36 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
+        sass: { 
+            dist: {
+                options: {
+                    style: 'compressed',
+                    trace: true,
+                    check: true,
+                    quiet: false
+                },
+                files: {
+                    'app.css': 'app.scss',
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-sass');
 
 
+    grunt.registerTask('default', ['uglify', 'grunt-contrib-sass']);
 
-grunt.loadNpmTasks('grunt-contrib-sass');
+
+};
