3832621cb19c50c29f5a656b6d3631cba5ccd4e7
AndriiKr <1drun1@gmail.com>
2016-11-01 14:34:12 +0200

    angular in process

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-	
+"use strict"
+  
+  var app = angular.module('app', ['ngSanitize']);
+
+  app.controller('AppController', ['$scope', '$sce', function($scope, $sce) {
+    $scope.username = "Andrey";
+    $scope.htmlData = "Hello <strong>Beach!</strong>";
+    $scope.posters = POSTERS;
+    $scope.quantity = 10;
+  }]);
+
+
+
+//ng-bind-html=""
+//ng-repeat="item in posters"
