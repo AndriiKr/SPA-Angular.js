2d60fb9be8545cb910336e13fce1ae26aeee965d
AndriiKr <1drun1@gmail.com>
2016-11-07 11:46:55 +0200

    new angular js logic

--- app.js
+++ app.js
@@ -21,20 +21,4 @@
     $scope.setActive = function(menuItem) {
     $scope.activeItem = menuItem
  }
-
-   $scope.task = function(taskItem){
-    alert("Task item is "+taskItem);
-  };
-  $scope.taskTitle = function(taskItem){
-    var elem = document.getElementById('title');
-    return elem.innerHTML = taskItem;
-  };
-    $scope.taskDeskr = function(taskItem){
-    var elem = document.getElementById('deskr');
-    return elem.innerHTML = taskItem;
-  };
-    $scope.taskImg = function(taskItem){
-    var elem = document.getElementById('image');
-    return elem.setAttribute( "src", "resources" + taskItem);
-  };
   }]);
